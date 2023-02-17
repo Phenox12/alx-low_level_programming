7d7a1ed5bb2d7e52d09ee3a43b3b43919e695c6c
Phenox12 <john4000360@gmail.com>
2023-02-16 21:25:07 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,8 +4,8 @@
  * main - A c program that print a line to the standard error
  * Return 1 (success)
  */
-int main(void)
+ int main(void)
 {
-	fput("and that piece of art is usefu\" - Dora Korpar, 2015-10-19\r", stdout);
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\r", stdout);
 	return (1);
 }
